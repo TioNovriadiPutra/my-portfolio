83b4fc64ecdee201556317abb75d775ec1b1ecf3
TioNovriadiPutra <tionvriadi@gmail.com>
2023-10-04 23:34:39 +0700

    chore(ProfilePicture): adding assets for profile picture

--- src/components/atoms/ProfilePicture.jsx
+++ src/components/atoms/ProfilePicture.jsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { colors } from "@themes/colors";
 
 const ProfilePicture = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@assets/images/profile.jpg")}
+        style={styles.image}
+      />
+    </View>
+  );
 };
 
 export default ProfilePicture;
@@ -15,5 +22,11 @@ const styles = StyleSheet.create({
     borderRadius: "50%",
     backgroundColor: colors.BlurWhite,
     marginBottom: 24,
+  },
+  image: {
+    width: 250,
+    height: 250,
+    borderRadius: "50%",
+    resizeMode: "contain",
   },
 });
