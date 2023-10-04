f66501cef4bb8ad1be3362ca68f588cdf04e6aa1
TioNovriadiPutra <tionvriadi@gmail.com>
2023-10-04 23:47:29 +0700

    style(Profile): adding animation for the profile picture, name, and status

--- src/components/atoms/ProfilePicture.jsx
+++ src/components/atoms/ProfilePicture.jsx
@@ -1,15 +1,39 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
 import { colors } from "@themes/colors";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const ProfilePicture = () => {
+  const translateYAnim = useSharedValue(-50);
+  const opacityAnim = useSharedValue(0);
+
+  const pictureAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateYAnim.value }],
+      opacity: opacityAnim.value,
+    };
+  });
+
+  const handleIn = () => {
+    translateYAnim.value = withTiming(0, { duration: 1000 });
+    opacityAnim.value = withTiming(1, { duration: 1000 });
+  };
+
+  useEffect(() => {
+    handleIn();
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, pictureAnimatedStyle]}>
       <Image
         source={require("@assets/images/profile.jpg")}
         style={styles.image}
       />
-    </View>
+    </Animated.View>
   );
 };
 
